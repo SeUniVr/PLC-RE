7426529af247f0cc5551983b77b2db47f751d662
Youssef Driouich <driysf@gmail.com>
2022-09-10 03:46:30 +0200

    Add files via upload

---
+++ runChartPlots.py
@@ -0,0 +1,16 @@
+from array import array
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+
+df = pd.read_csv('Daikon_Invariants/PLC_Dataset.csv')
+
+for x in range(1,len(sys.argv)):
+
+	data = pd.DataFrame(df,columns=[str(sys.argv[x])])
+	data.plot()
+	plt.xlabel("Time")
+	plt.legend([str(sys.argv[x])],bbox_to_anchor = (0.5, 1.1))
+	plt.grid()
+	plt.show()
