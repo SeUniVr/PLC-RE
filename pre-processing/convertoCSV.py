4b6cc5b90ee0bbc9211480046beb18502985372f
Youssef Driouich <driysf@gmail.com>
2022-09-14 18:34:18 +0200

    modify paths

--- pre-processing/convertoCSV.py
+++ pre-processing/convertoCSV.py
@@ -5,8 +5,8 @@ import os
 import sys
 
 
-path_csv ='PLC_CSV/'
-path_daikon = 'Daikon_Invariants/'
+path_csv ='../PLC_CSV/'
+path_daikon = '../Daikon_Invariants/'
 isExistCSV = os.path.exists(path_csv)
 isExistDaikon = os.path.exists(path_daikon)
 
@@ -15,8 +15,8 @@ if not isExistCSV:
     
 if not isExistDaikon:
     os.mkdir(path_daikon)
-    os.mkdir('Daikon_Invariants/')
-    f = open("Daikon_Invariants/Inv_conditions.spinfo", "a")
+    os.mkdir('../Daikon_Invariants/')
+    f = open("../Daikon_Invariants/Inv_conditions.spinfo", "a")
     f.write("PPT_NAME aprogram.point:::POINT\n VAR1 > VAR2")
     f.close()
 
@@ -43,7 +43,7 @@ def flatten_json(nested_json: dict, exclude: list=[''], sep: str='_') -> dict:
 
 def convert(plc) :
     # list of json files
-    files = sorted(glob.glob('historian/plc'+str(plc)+'*.json'))
+    files = sorted(glob.glob('../historian/plc'+str(plc)+'*.json'))
 
 
     # list to add dataframe from each file
@@ -76,7 +76,7 @@ def convert(plc) :
 
          
     print(df)
-    df.to_csv(r'PLC_CSV/PLC'+str(plc)+'Dataset.csv', index = False)
+    df.to_csv(r'../PLC_CSV/PLC'+str(plc)+'Dataset.csv', index = False)
 for x in range(1,int(sys.argv[1])+1):         
     convert(x)
 
